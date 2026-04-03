120dbcb924f5a64e03d29e7b79ee234a81fcd239
abdullah <abdullahramadan661@gmail.com>
2026-04-03 09:03:29 +0200

    Modified the dag to save the output using dynamic paths

--- dags/youm7_dag.py
+++ dags/youm7_dag.py
@@ -22,21 +22,29 @@ PARQUET_DATA_PATH = f"{BASE_PATH}/data/intermediate/new_urgent.parquet"
     catchup=False)
 def youm7_scraping_pipline():
 
+    # We define the category we want to run
+    # in the future, you can loop over a list of categories and create a task for each category
+    category_name = "urgent"
+
+    # define the dynamic paths using airflow macros
+    raw_file = f"{BASE_PATH}/data/raw/{category_name}_{{{{ ts_nodash }}}}.jsonl"
+    parquet_file = f"{BASE_PATH}/data/intermediate/{category_name}_{{{{ ts_nodash }}}}.parquet"
+
     # Task 1: Scrapy
     run_spider = BashOperator(
         task_id="run_scrapy_spider",
-        bash_command=f"cd {SCRAPY_DIR} && scrapy crawl {SPIDER_NAME} -o {RAW_DATA_PATH}")
+        bash_command=f"cd {SCRAPY_DIR} && scrapy crawl {SPIDER_NAME} -a category={category_name} -o {raw_file}")
 
     
     # Task 2: Convert jsonl to parquet
     @task
-    def convert_jsonl_to_parquet():
+    def convert_jsonl_to_parquet(input_path, output_path):
         from task_scripts.jsonl_to_parquet import run_conversion_function
-        run_conversion_function(RAW_DATA_PATH, PARQUET_DATA_PATH)
-    
+        run_conversion_function(input_path, output_path)
 
 
 
-    run_spider >> convert_jsonl_to_parquet()
+
+    run_spider >> convert_jsonl_to_parquet(input_path=raw_file, output_path=parquet_file)
 
 youm7_scraping_pipline()
